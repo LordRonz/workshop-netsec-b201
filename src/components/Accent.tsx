d602786b7ea0409a518a6766dd16f00e3cd765bc
LordRonz <aarontanhar2000@gmail.com>
2022-01-24 22:06:12 +0700

    feat: add modul pdf

--- src/components/Accent.tsx
+++ src/components/Accent.tsx
@@ -8,7 +8,7 @@ const Accent = ({ children, className }: AccentType) => {
       className={clsx(
         'transition-colors',
         // 'from-primary-300/40 to-primary-400/40 via-primary-300/40',
-        'text-transparent bg-clip-text bg-gradient-to-tr from-primary-300 to-primary-100',
+        'text-transparent bg-clip-text bg-gradient-to-tr from-primary-300 to-primary-100 dark:print:text-white print:text-black',
         className
       )}
     >
