018bf675c521e03b2e7a0d7b441efbe97ede16fb
LordRonz <aarontanhar2000@gmail.com>
2022-06-02 23:35:59 +0700

    feat: cypress config :rocket:

---
+++ cypress/e2e/pages/index.cy.ts
@@ -0,0 +1,14 @@
+import { beforeEach, cy, describe, it } from 'local-cypress';
+
+describe('Index Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display correct heading', () => {
+    cy.get('h1').should(
+      'contain',
+      'Modul Workshop Network and Security Lab B201'
+    );
+  });
+});
